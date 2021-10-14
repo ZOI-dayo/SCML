f0805ed25f71ce25485d82964569f2bbd6621f08
ZOI-dayo <64856587+ZOI-dayo@users.noreply.github.com>
2021-10-14 09:19:52 +0900

    `$ npx scml init` の実装

--- src/func/init/init.ts
+++ src/func/init/init.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import path from "path";
 import {Func} from "../func";
 
 export class InitFunc extends Func {
@@ -9,7 +10,44 @@ export class InitFunc extends Func {
     }
 
     override run(currentPath: string, argv: string[]) {
-        console.log(fs.readdirSync(currentPath));
-        console.log("init");
+        const dirMap: string[] = [
+            "assets",
+            "components",
+            "pages",
+            "static"
+        ];
+        for (const dirName of dirMap) {
+            const dirPath: string = path.join(currentPath, dirName.replace('/', path.sep));
+            if (!fs.existsSync(dirPath)) {
+                console.log("creating \"" + dirName + "\" directory...");
+                fs.mkdirSync(dirPath);
+            } else {
+                console.log("skip \"" + dirName + "\" directory...");
+            }
+        }
+        const fileMap: { [key: string]: string } = {
+            "components/MyComponent.html":
+                "<header>\n" +
+                "  <span>This is header</span>\n" +
+                "</header>\n" +
+                "<style scoped>\n" +
+                "header {\n" +
+                "  width: 100%;\n" +
+                "  background-color: azure;\n" +
+                "}\n" +
+                "</style>\n",
+            "pages/index.html":
+                "<MyComponent />\n",
+        };
+        for (const fileName in fileMap) {
+            const filePath: string = path.join(currentPath, fileName.replace('/', path.sep))
+            if (!fs.existsSync(filePath)) {
+                console.log("creating \"" + fileName + "\" ...");
+                fs.writeFileSync(filePath, fileMap[fileName], {flag: "wx"});
+            } else {
+                console.log("skip \"" + fileName + "\" directory...");
+            }
+        }
+        console.log("finished.");
     }
 }
